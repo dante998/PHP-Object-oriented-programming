b6888af8b3f567ab4e97a460483b5ee2fa8bfa46
Mihael Kamenov <Mihael@example.com>
2023-03-15 11:04:38 +0200

    deleted unnecessary files

--- function.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists('printSomething')) {
-  function printSomething(): void {
-    echo "Nothing to do.";
-  }
-}
-printSomething();
--- list.php
+++
@@ -1,7 +0,0 @@
-<?php
-function smallNumbers(): array {
-  return [10, 20, 30, 40, 50];
-}
-
-[$a, $b, $c, $d, $e] = smallNumbers();
-echo "$a,$b,$c,$d,$e";
